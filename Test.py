27231d8307a997678aecdc75a5a0f404a63699db
code3257 <132523885+code3257@users.noreply.github.com>
2023-05-03 16:42:18 -0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,30 @@
+print ("Test Program 1")
+
+learning = (100)
+
+name = input("What is your name?")
+
+if name == "Cody":
+ print("Hey Cody!")
+if learning >= 100:
+ print("good job, " + name)
+
+age = input("What is your age?")
+
+
+print("Howdy " + age + " year old " + name + f" You have a score of {learning}")
+
+
+#number1 = int(input("What is your favortite number?"))
+inputString = input("What is your favortite number?")
+
+
+
+if float(inputString) < 10:
+  print("What a small number")
+
+
+
+
+if number1<=10:
+ print("you gave a number below 10! Have more imagination")
---
+++ try.py
@@ -0,0 +1,4 @@
+number = input("What is your favortite number?")
+
+if number < 10:
+ print("you gave a number below 10! Have more imagination")
